493a7fb98f2e0bd478e2b16232806b2cb89eb9ad
onlyone-odoo <matiasb@onlyone.com.ar>
2025-07-18 10:56:06 -0300

    nueva accion 2

--- models/stock_lot.py
+++ models/stock_lot.py
@@ -1,7 +1,5 @@
 from odoo import fields, models
 
-from odoo import fields, models
-
 
 class StockLot(models.Model):
     _inherit = "stock.lot"
@@ -14,21 +12,13 @@ class StockLot(models.Model):
 
     def open_x_documento_spreadsheet(self):
         self.ensure_one()
-        if not self.x_documento:
-            return
-        try:
-            action = self.env.ref("spreadsheet.spreadsheet_action_open").read()[0]
-        except ValueError:
-            action = {
-                "type": "ir.actions.act_window",
-                "res_model": "documents.document",
-                "view_mode": "form",
-                "views": [(False, "form")],
-                "res_id": self.x_documento.id,
-                "target": "current",
-            }
-        action["context"] = {
-            "active_id": self.x_documento.id,
-            "default_res_id": self.x_documento.id,
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Documentos",
+            "res_model": "documents.document",
+            "view_mode": "kanban,list,form",
+            "views": [(False, "kanban"), (False, "list"), (False, "form")],
+            "domain": [("name", "=", self.name)],
+            "context": {"search_default_name": self.name},
+            "target": "current",
         }
-        return action
