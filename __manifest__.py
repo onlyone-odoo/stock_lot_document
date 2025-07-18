8f6d837d1734240f64f26511641b7c684968d94e
onlyone-odoo <matiasb@onlyone.com.ar>
2025-07-18 10:20:39 -0300

    nueva accion

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://onlyone.odoo.com/",
     "license": "AGPL-3",
     "category": "Inventory",
-    "version": "17.0.1.0.0",
+    "version": "17.0.2.1.0",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
@@ -16,7 +16,7 @@
         "python": [],
         "bin": [],
     },
-    "depends": ["stock", "documents"],
+    "depends": ["stock", "documents", "spreadsheet"],
     "data": [
         "views/stock_lot_form.xml",
     ],
--- models/stock_lot.py
+++ models/stock_lot.py
@@ -1,23 +1,34 @@
 from odoo import fields, models
 
+from odoo import fields, models
+
+
 class StockLot(models.Model):
-    _inherit = 'stock.lot'
+    _inherit = "stock.lot"
 
     x_documento = fields.Many2one(
-        comodel_name='documents.document',
-        string='Documento',
-        domain="[('name', '=', name)]"
+        comodel_name="documents.document",
+        string="Documento",
+        domain="[('name', '=', name)]",
     )
 
-    def open_x_documento(self):
+    def open_x_documento_spreadsheet(self):
         self.ensure_one()
         if not self.x_documento:
             return
-        return {
-            'type': 'ir.actions.act_window',
-            'res_model': 'documents.document',
-            'view_mode': 'form',
-            'views': [(False, 'form')],
-            'res_id': self.x_documento.id,
-            'target': 'current',  # Abre en la ventana actual, como en Kanban
+        try:
+            action = self.env.ref("spreadsheet.spreadsheet_action_open").read()[0]
+        except ValueError:
+            action = {
+                "type": "ir.actions.act_window",
+                "res_model": "documents.document",
+                "view_mode": "form",
+                "views": [(False, "form")],
+                "res_id": self.x_documento.id,
+                "target": "current",
+            }
+        action["context"] = {
+            "active_id": self.x_documento.id,
+            "default_res_id": self.x_documento.id,
         }
+        return action
