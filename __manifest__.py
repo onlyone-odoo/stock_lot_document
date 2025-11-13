3c550101fc8a8157097754bf90bc34ba53a450f3
onlyone-odoo <matiasb@onlyone.com.ar>
2025-11-13 20:26:36 -0300

    fix wizard menu

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://onlyone.odoo.com/",
     "license": "AGPL-3",
     "category": "Inventory",
-    "version": "18.0.4.3.1",
+    "version": "18.0.4.4.1",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
