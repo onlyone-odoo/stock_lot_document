c2601463c3093bc2113a2f2b2f4023942c40508a
onlyone-odoo <matiasb@onlyone.com.ar>
2025-11-29 15:40:33 -0300

    list view button + translate

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://onlyone.odoo.com/",
     "license": "AGPL-3",
     "category": "Inventory",
-    "version": "18.0.7.6.1",
+    "version": "18.0.7.7.1",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
