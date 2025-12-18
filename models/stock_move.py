e0e3e2b5798325f832b65fbdf5cfe92d498481ef
onlyone-odoo <matiasb@onlyone.com.ar>
2025-12-18 14:46:43 -0300

    refactor first_stock_lot_id

--- models/stock_move.py
+++ models/stock_move.py
@@ -1,13 +1,25 @@
-from odoo import models, _
+from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
 
+    first_lot_id = fields.Many2one(
+        "stock.lot",
+        string="First Lot",
+        compute="_compute_first_lot_id",
+        store=False,
+    )
+
+    @api.depends("lot_ids")
+    def _compute_first_lot_id(self):
+        for move in self:
+            move.first_lot_id = move.lot_ids[0] if move.lot_ids else False
+
     def open_lot_documento_spreadsheet(self):
-        """Delegate to open the linked document in Spreadsheet from the lot."""
+        """Delegate to open the linked document in Spreadsheet from the first lot."""
         self.ensure_one()
-        if not self.lot_id:
+        if not self.first_lot_id:
             raise UserError(_("No lot selected in this line."))
-        return self.lot_id.open_x_documento_spreadsheet()
+        return self.first_lot_id.open_x_documento_spreadsheet()
