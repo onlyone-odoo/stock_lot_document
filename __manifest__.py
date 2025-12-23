bf737825c6040041233ad2dae6e00cc30a1e7f78
onlyone-odoo <matiasb@onlyone.com.ar>
2025-12-23 11:02:11 -0300

    add menu in sale

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://onlyone.odoo.com/",
     "license": "AGPL-3",
     "category": "Inventory",
-    "version": "18.0.9.8.5",
+    "version": "18.0.9.9.5",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
@@ -27,5 +27,6 @@
     "data": [
         "views/stock_lot_form.xml",
         "views/mrp_views.xml",
+        "views/sale_lot_menu.xml",
     ],
 }
